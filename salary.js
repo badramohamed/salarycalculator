480dca5eecf453b61032e6504be2d54dd3bcaddb
Badra Mohamed <moha1150@umn.edu>
2022-06-02 00:37:27 -0500

    added delete & push

--- salary.js
+++ salary.js
@@ -1,40 +1,60 @@
 console.log('in salary ')
 
-$(document).ready(readyNow)
+$(document).ready(onReady)
 
 let totalMonthly= [];
 
-function readyNow(){
-    console.log('in readyNow')
-   
-    $('#submitBtn').on('click', employeeSalary);
-   
-    $('#employeeSal').on('click', '#deleteBtn', deleteInput);
-
-
-    function employeeSalary(){
-        let firstName = $('#firstName').val();
-        let lastName = $('#lastName').val();
-        let id = $('#id').val();
-        let title = $('#title').val();
-        let annualSalary = $('#annualSalary').val();
+function onReady(){
+console.log('in onReady');
     
-      // append to dom
-      $('#employeeSal').append(`
-      <tr>
-           <td>${firstName}</td>
-          <td>${lastName}</td>
-          <td>${id}</td>
-          <td>${title}</td>
-          <td>$${annualSalary}</td>
-          <th><button id="deleteBtn">Delete</button></th>
-      </tr> 
-      `)
-      
-          $('#firstName').val('');
-          $('#lastName').val('');
-          $('#id').val('');
-          $('#title').val('');
-          $('#annualSalary').val('');
+$('#submitBtn').on('click', employeeSalary);
+    
+$('#employeeSal').on('click', '#deleteBtn', deleteInput);
+    
+    
+function employeeSalary(){
+let firstName = $('#firstName').val();
+let lastName = $('#lastName').val();
+let id = $('#id').val();
+let title = $('#title').val();
+let annualSalary = $('#annualSalary').val();
+        
+// append to dom
+ $('#employeeSal').append(`
+    <tr>
+     <td>${firstName}</td>
+     <td>${lastName}</td>
+     <td>${id}</td>
+     <td>${title}</td>
+     <td>$${annualSalary}</td>
+     <th><button id="deleteBtn">Delete</button></th>
+    </tr> 
+    `)
+        
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#title').val('');
+    $('#annualSalary').val('');
+        
+// push our montly into annual
+    totalMonthly.push(annualSalary);
+     let totalSalary = 0;
+     for (let salary of totalMonthly){
+       totalSalary += Number(salary);
+    }
+
+ if (totalSalary/12 >20000){
+$('#totalMonthly').text(`Total Monthly: $ ${(totalSalary/12).toFixed(2)}`).css('background-color', 'red');
+}
+ 
+$('#totalMonthly').text(`Total Monthly: $ ${(totalSalary/12).toFixed(2)}`);
+        
     }
 }
+// delete function
+function deleteInput(){
+    $(this).parent().parent().remove();
+    
+}
+
